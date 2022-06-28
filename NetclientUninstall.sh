d41426ea6b09d64a097102257eacb49d46caf57d
adamphetamine <47110465+adamphetamine@users.noreply.github.com>
2022-06-28 17:52:57 +1000

    Update NetclientUninstall.sh
    add lines to remove binaries and receipts from new names and new locations

--- NetclientUninstall.sh
+++ NetclientUninstall.sh
@@ -11,9 +11,17 @@ rm /usr/local/bin/netclient
 rm /usr/local/bin/wg
 rm /usr/local/bin/wireguard-go
 
+# remove old bits from new location
+rm /usr/sbin/netclient
+rm /usr/sbin/wg
+
 # remove receipts
 rm /private/var/db/receipts/com.gravitl.pkg.NetclientWireguard.bom
 rm /private/var/db/receipts/com.gravitl.pkg.NetclientWireguard.plist
+
+# remove new name receipts
+rm /private/var/db/receipts/com.gravitl.pkg.netclient.bom
+rm /private/var/db/receipts/com.gravitl.pkg.netclient.plist
 
 # remove bits from the Netmaker standard install that doesn't work on macOS
 rm -rf /private/etc/netclient
