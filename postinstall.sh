c6287009ce9973c1acf4ce9df4c51ae7bc31a437
adamphetamine <47110465+adamphetamine@users.noreply.github.com>
2022-06-28 17:55:02 +1000

    Update postinstall.sh
    updated for new location of netclient binary from /usr/local/netclient/netclient to /usr/sbin/netclient

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin 
 # Patch launchd so it works- this is to cope with the binary location changing from /etc to /usr/local... 
-sudo sed -i '' 's|/etc/netclient/netclient|/usr/local/bin/netclient|g' /Library/LaunchDaemons/com.gravitl.netclient.plist 
+sudo sed -i '' 's|/etc/netclient/netclient|/usr/sbin/netclient|g' /Library/LaunchDaemons/com.gravitl.netclient.plist 
 # Load new launchd launchctl 
 load -w /Library/LaunchDaemons/com.gravitl.netclient.plist &>/dev/null 
 # Need to enable it so it launches at startup? 
