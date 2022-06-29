2de8dc93408d69b37b5e1be65dcbe4e00fbb1f38
adamphetamine <47110465+adamphetamine@users.noreply.github.com>
2022-06-29 18:30:54 +1000

    Update postinstall.sh
    change /usr/local/netclient/wg to /usr/local/netclient/wireguard-go because I misremembered which binary we need

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin 
 # symlink new directory contents so it's in $PATH
 sudo ln -s /usr/local/netclient/netclient /usr/local/bin
-sudo ln -s /usr/local/netclient/wg /usr/local/bin
+sudo ln -s /usr/local/netclient/wireguard-go /usr/local/bin
 # Patch launchd so it works- this is to cope with the binary location changing from /etc to /usr/local... 
 sudo sed -i '' 's|/etc/netclient/netclient|/usr/local/netclient/netclient|g' /Library/LaunchDaemons/com.gravitl.netclient.launchd.plist 
 # Load new launchd launchctl 
