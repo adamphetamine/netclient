bb011c0061dd74c53d8651f7e9364c7b91ab5bb9
adamphetamine <47110465+adamphetamine@users.noreply.github.com>
2022-10-20 16:25:34 +1100

    Update postinstall.sh
    Updated Packages.app so the install path is now
    /usr/local/bin 
    instead of 
    /usr/local/netclient
    
    This should mean that the $PATH linking is not needed, but I've added a command for 'WireGuard-go' to this script, even though it is commented out. Supposedly WireGuard-tools ('wg') are no longer required for netclient to work, but the developers are still including the binary...
    So I'm adding everything back in, with hopes of glorious reliability, comrade.

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin 
 # symlink new directory contents so it's in $PATH
-sudo ln -s /usr/local/netclient/netclient /usr/local/bin
-sudo ln -s /usr/local/netclient/wg /usr/local/bin
+# sudo ln -s /usr/local/netclient/netclient /usr/local/bin
+# sudo ln -s /usr/local/netclient/wg /usr/local/bin
+# sudo ln -s /usr/local/netclient/wireguard-go /usr/local/bin
 # Patch launchd so it works- this is to cope with the binary location changing from /etc to /usr/local... 
-sudo sed -i '' 's|/etc/netclient/netclient|/usr/local/netclient/netclient|g' /Library/LaunchDaemons/com.gravitl.netclient.launchd.plist 
+sudo sed -i '' 's|/etc/netclient/netclient|/usr/local/bin/netclient|g' /Library/LaunchDaemons/com.gravitl.netclient.launchd.plist 
 # Load new launchd launchctl 
 load -w /Library/LaunchDaemons/com.gravitl.netclient.plist &>/dev/null 
 # Need to enable it so it launches at startup? 
